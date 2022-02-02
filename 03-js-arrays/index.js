4575a099d259a388a325eb37814dea5d33bf5e4c
Aysan Isayo <aisayo@icloud.com>
2022-02-02 11:52:52 -0500

    lecture starter code

---
+++ 03-js-arrays/index.js
@@ -0,0 +1,67 @@
+// Understanding arrays
+
+// What are arrays and how do they allow us to structure our data?
+
+// Define a variable 'pokemons' set to an empty array.
+
+// Rewrite the 'pokemons' array with the following elements: 'pikachu', 'bulbasour', 'jigglypuff'
+
+// Elements:
+
+//  Return 'bulbasour' from the pokemons array (challenge: there are 2 different ways to return this element)
+
+// Modifying elements:
+
+// Add a new element 'mew' to the end of pokemons using a non-destructive method.
+
+// Remove the first element of 'pokemons' in a non-destructive manner
+
+// Create a new array, 'favorites', that includes the first 2 elements from pokemons only and the following 2 new elements: 'squirtle', 'turtwig'. This should be non-destructive.
+
+// For the following exercises, comment in the following array:
+
+// let pokemons = ["Bulbasaur","Ivysaur","Venusaur","Charmander","Charmeleon","Charizard","Squirtle","Wartortle","Blastoise","Caterpie","Metapod","Butterfree","Weedle","Kakuna","Beedrill","Pidgey","Raticate","Spearow","Fearow","Ekans","Arbok","Pikachu","Raichu","Sandshrew","Sandslash","Nidoran","Nidorina","Jigglypuff","Wigglytuff","Zubat","Golbat","Oddish","Gloom","Vileplume","Paras","Parasect","Venonat","Venomoth","Diglett","Dugtrio","Meowth","Persian","Psyduck","Golduck","Mankey","Primeape","Growlithe","Arcanine"]
+
+// Iteration
+
+// looping methods:
+
+// for loop
+// Define a for loop that will log each pokemon character to the console.
+
+// for..of
+// Rewrite the previous exercise using a for..of loop
+
+// Can also use 'do...while', and 'while' loops to cycle through array's elements.
+
+// iteration methods:
+
+// .forEach
+
+// arr.forEach(function(el){
+//     console.log(el)
+// })
+
+// Arrow function syntax:
+// arr.forEach(el => console.log(el))
+
+// Use forEach to iteratate through the pokemons array and return each character name in large caps
+
+// Searching an array: .find() vs .filter()
+
+// Create a method that will return all pokemon characters that start with the letter 's'. Which array method should be used for this task?
+
+// Modifying an array
+
+// .map()
+
+// arr.map(function(el){
+//     // change each element
+// })
+
+// arr.map(el => {
+// change each element
+// })
+
+// Using map, return a new array with all pokemon character names lowercased.
+
