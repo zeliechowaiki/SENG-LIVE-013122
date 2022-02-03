51633ca213639e4145b562fc4bab62109a0514a9
Aysan Isayo <aisayo@icloud.com>
2022-02-03 16:18:05 -0500

    js objects solution code

--- 03-js-arrays/index.js
+++ 03-js-arrays/index.js
@@ -4,7 +4,6 @@
 // let increaseLikes = () => {
 //     console.log(++likes)
 // }
-
 // likeBttn.addEventListener("click", increaseLikes)
 
 // Understanding arrays
@@ -29,7 +28,7 @@
 
 // For the following exercises, comment in the following array:
 
-// let pokemons = ["Bulbasaur","Ivysaur","Venusaur","Charmander","Charmeleon","Charizard","Squirtle","Wartortle","Blastoise","Caterpie","Metapod","Butterfree","Weedle","Kakuna","Beedrill","Pidgey","Raticate","Spearow","Fearow","Ekans","Arbok","Pikachu","Raichu","Sandshrew","Sandslash","Nidoran","Nidorina","Jigglypuff","Wigglytuff","Zubat","Golbat","Oddish","Gloom","Vileplume","Paras","Parasect","Venonat","Venomoth","Diglett","Dugtrio","Meowth","Persian","Psyduck","Golduck","Mankey","Primeape","Growlithe","Arcanine"]
+ let pokemons = ["Bulbasaur","Ivysaur","Venusaur","Charmander","Charmeleon","Charizard","Squirtle","Wartortle","Blastoise","Caterpie","Metapod","Butterfree","Weedle","Kakuna","Beedrill","Pidgey","Raticate","Spearow","Fearow","Ekans","Arbok","Pikachu","Raichu","Sandshrew","Sandslash","Nidoran","Nidorina","Jigglypuff","Wigglytuff","Zubat","Golbat","Oddish","Gloom","Vileplume","Paras","Parasect","Venonat","Venomoth","Diglett","Dugtrio","Meowth","Persian","Psyduck","Golduck","Mankey","Primeape","Growlithe","Arcanine"]
 
 // Iteration
 
@@ -50,17 +49,44 @@
 // arr.forEach(function(el){
 //     console.log(el)
 // })
+// let pokemons = ["pikachu", "bulbosaur", "charmander"]
 
 // Arrow function syntax:
 // arr.forEach(el => console.log(el))
 
+let x = []
+pokemons.forEach(el => {
+    x.push(el.toUpperCase())
+})
+
+console.log(x)
+
+// const total3 = books.forEach(logTitle)
+
+// function logTitle(item){
+//   let newItem1 = [];
+//   newItem1.push(item.title.toUpperCase();
+//   return newItem1
+// }
+
+// console.log(total3)
+
 // Use forEach to iteratate through the pokemons array and return each character name in large caps
 
 // Searching an array: .find() vs .filter()
+arr = ["cat", "fish", "dog", "dinosaur"]
+// console.log(arr.find((el) =>  el.startsWith('d')))
+
+// let results = arr.filter((el) => el.startsWith('d'))
+
+// console.log(results)
 
 // Create a method that will return all pokemon characters that start with the letter 's'. Which array method should be used for this task?
 
-// Modifying an array
+// let results = pokemons.filter(char => char.startsWith('S'))
+// console.log(results)
+
+// Modifying an arrays elements
 
 // .map()
 
@@ -68,9 +94,16 @@
 //     // change each element
 // })
 
-// arr.map(el => {
+arr.map(el => {
 // change each element
-// })
+})
 
 // Using map, return a new array with all pokemon character names lowercased.
 
+// let newArr = pokemons.map(el => el.toLowerCase())
+
+let newArr = pokemons.map(function(el){
+    return el.toLowerCase() + " test"
+})
+
+console.log(newArr)
--- 04-js-objects/index.js
+++ 04-js-objects/index.js
@@ -4,6 +4,8 @@
 
 // Empty object
 
+// let pokemon = {}
+
 // Object with many properties
 
 // Define a character variable and assign it to an object containing name, img, and likes keys. The value of likes should be 0 while name and img can be whatever pokemon character data you like.
@@ -17,10 +19,33 @@ const character = {
 // Retreiving property values from an object
 
 // bracket notation
+// let name = 'name'
+// console.log(character['name'])
+// console.log(character[name])
 
 // dot notation
+// limitations: cant use multi worded keys, need to know the literal name of the key, cant be used for variables
+
+// let key = prompt("what do you want to know about the character")
+
+// console.log(character[key])
+
+// console.log(character.name)
 
 // Define a function increaseValue() that should take in 2 arguments: object and key. Increment the value by 1 so that every time the function is invoked, the number of likes will increment.
+
+// function increaseValue(obj, key) {
+  
+//   return ++obj[key]
+//  }
+
+
+// const increaseValue = (obj, key) => obj[key] += 1
+
+
+// console.log(increaseValue(character, "likes"))
+// console.log(increaseValue(character, "likes"))
+
 
 // Example: increaseValue(character, "likes") -> 1
 // Example: increaseValue(character, "likes") -> 2
@@ -32,17 +57,58 @@ const character = {
 
 // destructive
 
+// character.abilities = "thunderbolt"
+// character["abilities"] = "thunderbolt"
+
+// console.log(character)
+
 // non-destructive
 
-// Create a function updateChar(): this function should take in three arguments: an character Object, a key and a value. This function should operate non-destructively so it should return a new Object that has the new property included.
+// spread operator 
+
+// let updatedChar = {...character, abilities: "testing"}
+
+// Object.assign()
+
+// let updatedChar = Object.assign({}, character, {abilites: "thunderbolt"})
+
+// console.log(updatedChar)
+// console.log(character)
+
+// Create a function updateChar(): this function should take in three arguments: an object, a key and a value. This function should operate non-destructively so it should return a new Object that has the new property included.
 
 // Example: updateChar(character, "abilities", []) -> {name: 'Pikachu', img: 'www.img.com', likes: 0, abilities: []}
+
+// function updateChar(obj,key,value){
+//   return {...obj, [key]:value}
+// }
+
+// const updateChar = (obj, key, value) => Object.assign({}, obj, {[key]: value})
+
+
+// console.log(updateChar(character, "abilities", []))
+// console.log(character)
+
+
 
 // Removing a key/value pair from an existing object
 
 // destructive
 
+// delete character.img
+
+// console.log(character)
+
 // non-destructive
+
+// make a new obj using the spread operator 
+// deleting the key/value pair from the new obj 
+
+let newObj = {...character}
+delete newObj.img
+
+console.log(newObj)
+console.log(character)
 
 // for...in loop
 
@@ -50,7 +116,11 @@ const character = {
 
 // Example: charDetails(character) -> name: Pikachu img: www.img.com likes: 0
 
-// for...of loop
+// for...in loop
+
+// for (const key in character){
+//   console.log(character[key])
+// }
 
 // Think about the necessary steps to return the same result as above, using a for...of loop.
 
@@ -72,4 +142,23 @@ const pikachu = {
   ],
 };
 
+function printAbilities(obj){
+  let abilitiesArr = obj.abilities
+  abilitiesArr.forEach(aObj => {
+    for (const k in aObj){
+      console.log(aObj[k])
+    }
+  })
+}
+
+console.log(printAbilities(pikachu))
+
 // BONUS: Modify printAbilities so that it returns the string as: 'Abilities: static, lightning-rod'
+
+
+// let arr1 = ["cat", "dog"]
+
+// let arr2 = ["fish", "hamster"]
+
+// let combinedArr = arr1.concat(arr2)
+// console.log([...arr1, ...arr2])
